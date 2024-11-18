e70f716e91f39a22db80c196f79b2c996a65aa49
Andrés Salazar <salazarcode@gmail.com>
2024-11-17 22:39:17 -0400

    A ver si despliega

---
+++ DemoData.sql
@@ -0,0 +1,43 @@
+﻿insert into classes(uniquekey, isprimitive, name) 
+    values
+        ('string', true, 'Cadena de texto'),
+        ('integer', true, 'Número entero'),
+        ('real', true, 'Número real'),
+        ('char', true, 'Caracter'),
+        ('datetime', true, 'Fecha y hora'),
+        ('user', false, 'Usuario');
+
+insert into properties(classid, propertyclassid, uniquekey, name) 
+    values
+        (6, 1, 'first-name', 'Nombre'),
+        (6, 1, 'last-name', 'Apellido'),
+        (6, 2, 'age', 'Edad'),
+        (6, 5, 'birth-date', 'Fecha de nacimiento'),
+        (6, 5, 'father', 'Padre');
+
+insert into DeleteBehaviours(name) 
+values
+    ('ON_CASCADE'),
+    ('NULL'),
+    ('RESTRICT');    
+
+insert into AbstractPropertyDetails(PropertyID, Min, Max, DeleteBehaviour) 
+    VALUES
+        (5, 0, 1, 'ON_CASCADE');
+
+insert into objects(classid, createdat) values(6, now());
+insert into stringvalues(objectid, propertyid, value) 
+values
+    (1,1, 'Pedro'),
+    (1,2, 'Salazar'),
+    (1,3, '56'),
+    (1,4, '19680713');
+
+insert into objects(classid, createdat) values(6, now());
+insert into stringvalues(objectid, propertyid, value) 
+values
+    (2,1, 'Andrés'),
+    (2,2, 'Salazar'),
+    (2,3, '35'),
+    (2,4, '19890517'),
+    (2,5, '1');
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-En este documento defino que características tiene este proyecto.
+Este proyecto aspira a convertirse en un BPMS, sigue una arquitectura monolítica y en capas.
+
+En su capa de presentación tiene el API: mi api define requests con validaciones hechas con FluentValidations y requests, que definen lo único que verá el usuario. - Requests - FluentValidation - Responses - AutoMapper
+
+En mi capa de dominio
